5058f120b729df73d6f06c0875a4d22fd0a6814c
sxw <sxw34336@163.com>
2019-01-14 15:53:03 +0800

    缓存命中率+通信开销

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -6,9 +9,8 @@ import java.util.Map;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		int averagetime=0;
-		int avercc=0;
 		for(int i=0;i<10;i++){
 			dataProcess data=new dataProcess();
 			QuerySpace querySpace=new QuerySpace(2700,4900,20900,30800,200);
@@ -19,34 +21,29 @@ public class Main {
 			anonymizer.setUserIdentifiers(identifiers);
 			lbs.querySpace=querySpace;
 			long sumtime=0;
-			int radius=750;
-			int k=40;
-			int ccostu2a=0;//暂定通信开销
-			int ccostu2l=0;
-			int communicationcost=0;
+			int radius=500;
+			int k=60;
+			PrintWriter pw=new PrintWriter(new File("src/out.txt"));
 			for(User user:userList){
 				Map<String, Object> userMSG=user.generateMSG(radius, k, userList);
-				ccostu2a+=k;
+				pw.write(userMSG.toString());
 				long time1 = System.currentTimeMillis();
 				anonymizer.createAnonymityArea((List<Area>) userMSG.get("Region"));
 				Map<String, Object> MSGa2l=anonymizer.generateMSGA2L(userMSG);//匿名器生成信息
-				ccostu2l++;
+				pw.write(MSGa2l.toString());
 				long time2 = System.currentTimeMillis();
 				List<User> result=lbs.search(MSGa2l, userList);//LBS查询得到结果
-				ccostu2l+=result.size();
+				pw.write(result.toString());
 				long time3 = System.currentTimeMillis();
 				List<User> now=anonymizer.resultFilter(result);
-				ccostu2a++;
+				pw.write(now.toString());
 				//System.out.println(now.size());
 				long time4 = System.currentTimeMillis();
 				sumtime+=time4-time3+time2-time1;
-				communicationcost=ccostu2a+ccostu2l;
 			}
 			averagetime+=sumtime;
-			avercc+=communicationcost;
 		}
 		//System.out.println("运行时间:"+averagetime/10+" ms");
-		System.out.println("通信次数："+avercc/10);
 	}
 
 }
